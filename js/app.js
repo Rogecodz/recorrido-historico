ca295d0d762d977bcb7d0465a8cd880989ea1c58
Rogecodz <rogecodz@outlook.com>
2023-03-08 01:01:27 -0600

    Agregando contenido a linea del tiempo

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const observerLeft = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} else {
+			entry.target.classList.remove('show');
+		}
+	});
+});
+
+const observerRight = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		} else {
+			entry.target.classList.remove('show');
+		}
+	});
+});
+
+const hiddenElementsLeft = document.querySelectorAll('.hidden-left');
+hiddenElementsLeft.forEach((el) => observerLeft.observe(el));
+
+const hiddenElementsRight = document.querySelectorAll('.hidden-right');
+hiddenElementsRight.forEach((el) => observerRight.observe(el));
